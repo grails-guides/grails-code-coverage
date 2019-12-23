7dde8f337beed276f31c837521b6f353e98737e1
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-23 07:17:36 +0100

    fix NameService

--- initial/grails-app/services/demo/NameService.groovy
+++ initial/grails-app/services/demo/NameService.groovy
@@ -1,18 +1,22 @@
 package demo
 
+import groovy.transform.CompileStatic
+
+@CompileStatic
 class NameService {
 
     List<Name> findAllPersonNames() {
       Person.findAll().collect { Person person ->
-            String firstName =
+            String firstName = null
+            String lastName = null
             if ( person.name.contains('del ') ) {
-                String arr[] = person.name.split('del ')
+                String[] arr = person.name.split('del ')
                 if ( arr.length > 0 ) {
                     firstName = arr[0]
                     lastName = arr.getAt(0)
                 }
             }
-            String arr[] = person.name.split(' ')
+            String[] arr = person.name.split(' ')
 
 
             new Name(firstName: firstName, lastName: lastName)
